b0df0fc0798ca6b7f7e7b94a23ff8e47c7802296
Otti Daniel <33727291+Sir-Dazzling@users.noreply.github.com>
2020-04-29 19:18:45 +0100

    set up basic spring boot project structure

---
+++ src/main/java/com/dazzlingstar/springboot/restdemo/rest/DefaultController.java
@@ -0,0 +1,23 @@
+package com.dazzlingstar.springboot.restdemo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class DefaultController
+{
+    // expose "/" that return "Hello World"
+    @GetMapping("/")
+    public String sayHello()
+    {
+        return "Hello World! My name is Otti Daniel Onyedikachi, i love java and the Time on server is " + LocalDateTime.now();
+    }
+
+    @GetMapping("/messages")
+    public String getMessages()
+    {
+        return "Lol I have a crush on someone so beautiful";
+    }
+}
